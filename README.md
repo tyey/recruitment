86ea8a2dd3c2d1cf83a75e4127e419cd45fc9cd0
Tomasz Janik <tjanik@yotpo.com>
2023-01-13 13:53:29 +0100

    db initialization fix

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Recruitment project
+Get familiar with the project by quickly reviewing it structure.
 
-Build project with `./gradlew clean build`
+Your task is to:
+* Build project in terminal with `./gradlew clean build` and make sure build is stable.
+* Design the REST layer according to standards which you think are the most important. Follow clean code and architecture principles which you are using on daily basis.
+* Implement endpoints which:
+  * allow to get all reviews of a product,
+  * allow to add new review for existing product.
+* Add openAPI with swaggerUI to test your solution.
 
 Good Luck!
--- src/test/java/com/yotpo/recruitment/RecruitmentApplicationTests.java
+++ src/test/java/com/yotpo/recruitment/RecruitmentApplicationTests.java
@@ -2,7 +2,9 @@ package com.yotpo.recruitment;
 
 import com.yotpo.recruitment.model.Product;
 import com.yotpo.recruitment.model.Review;
+import com.yotpo.recruitment.model.Stars;
 import com.yotpo.recruitment.repositories.ProductRepository;
+import com.yotpo.recruitment.repositories.ReviewRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,9 @@ class RecruitmentApplicationTests {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    ReviewRepository reviewRepository;
+
     @Test
     void contextLoads() {
     }
@@ -29,6 +34,20 @@ class RecruitmentApplicationTests {
         Set<Review> reviews = product.get().getReviews();
         Assertions.assertNotNull(reviews);
         Assertions.assertEquals(5, reviews.size());
+    }
+
+    @Test
+    @Transactional
+    void shouldAddReview(){
+        Optional<Product> product = productRepository.findById(1L);
+        Review review = new Review();
+        review.setContent("test");
+        review.setStars(Stars.FOUR);
+        review.setProduct(product.get());
+
+        Review saved = reviewRepository.save(review);
+
+        Assertions.assertNotNull(saved.getId());
     }
 
 }
